0ef310189405499640a1cbd4ca060fab374e7c53
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2026-01-24 09:49:47 +0530

    completed the start of srticle

---
+++ app/blogDetails/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import { menuItems } from "@/app/menu/items";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function BlogDetails({ params }: Props) {
+  const article = menuItems.find((item) => item.slug === params.slug);
+
+  if (!article) return notFound();
+
+  return (
+    <div className="max-w-4xl mx-auto px-4 py-16">
+      <p className="text-gray-500 mb-2">{article.date}</p>
+
+      <h1 className="font-playfair text-4xl font-semibold mb-6">
+        {article.description}
+      </h1>
+
+      <img
+        src={article.img}
+        alt={article.description}
+        className="w-full h-[420px] object-cover rounded-xl mb-8"
+      />
+
+      <p className="text-gray-700 leading-7">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco.
+      </p>
+    </div>
+  );
+}
--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import { menuItems } from "../menu/items";
 
@@ -6,8 +7,7 @@ function Pages() {
   const router = useRouter();
 
   return (
-    <div className="py-20 bg-[#F9F9F7] ">
-      {/* TITLE */}
+    <div className="py-20 bg-[#F9F9F7]">
       <h2 className="font-playfair text-6xl font-semibold text-center mb-4">
         Our Blog & Articles
       </h2>
@@ -20,17 +20,21 @@ function Pages() {
       <div className="md:px-[40px] lg:px-[5rem] xl:px-[7.5rem] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {menuItems.map((item) => (
           <div
-            onClick={() => router.push("/book")}
             key={item.id}
-            className="bg-white border border-gray-300 rounded-xl shadow-sm overflow-hidden cursor-pointer hover:shadow-lg transition-shadow duration-300"
+            onClick={() => router.push(`/blogDetails/${item.slug}`)}
+            className="bg-white border border-gray-300 rounded-xl shadow-sm overflow-hidden cursor-pointer hover:shadow-lg transition-all"
           >
-            <img src={item.img} className="h-48 w-full object-cover" />
+            <img
+              src={item.img}
+              alt={item.description}
+              className="h-48 w-full object-cover"
+            />
 
             <div className="p-5">
               <p className="text-gray-500 text-sm font-semibold mb-1">
                 {item.date}
               </p>
-              <p className="text-lg">{item.discription}</p>
+              <p className="text-lg font-medium">{item.description}</p>
             </div>
           </div>
         ))}
